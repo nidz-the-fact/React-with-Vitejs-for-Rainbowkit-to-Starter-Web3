0ff78c4aec520f138bcb798a6b6f91d62aefacfe
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-10-23 11:26:51 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
 import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeScript } from '@chakra-ui/react'
 import theme from './theme'
-
 import '@rainbow-me/rainbowkit/styles.css';
 import {
   QueryClientProvider,
@@ -14,53 +12,34 @@ import {
 } from "@tanstack/react-query";
 import { WagmiProvider } from 'wagmi';
 import {
+  sepolia,
+  optimismSepolia,
+  thaiChain,
+  jbc,
+  jbcTestnet
+} from 'wagmi/chains';
+import {
   getDefaultConfig,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import {
-  jbc,
-} from 'viem/chains';
-// import {
-//   sepolia,
-//   optimismSepolia,
-// } from 'opstack-kit/chains';
-
-import { pomChaincustom } from './pomChaincustom.ts'
-// import { lavaChaincustom } from './lavaChaincustom.ts'
-
-
 const config = getDefaultConfig({
   appName: 'My RainbowKit App',
   projectId: import.meta.env.VITE_SOME_KEY, // .env
-  chains: [
-    jbc, 
-    pomChaincustom, 
-    // lavaChaincustom
-  ],
+  chains: [sepolia, optimismSepolia, thaiChain, jbc, jbcTestnet],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
-
 const queryClient = new QueryClient();
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
-
           <ChakraProvider>
             <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-
             <App />
-
           </ChakraProvider>
-
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
-
   </React.StrictMode>,
 )
